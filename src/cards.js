b5dfaf0626b710f78281971c35bcb70981a83760
Apmds <ap1mrfj@gmail.com>
2025-10-05 19:05:25 +0100

    cards e coisas misc

---
+++ src/cards.js
@@ -0,0 +1,31 @@
+function showTerraCard() {
+    document.getElementById("terraCard").classList.add('active');
+}
+
+
+function closeTerraCard() {
+    document.getElementById("terraCard").classList.remove('active');
+}
+
+
+
+function showAquaCard() {
+    document.getElementById("aquaCard").classList.add('active');
+}
+
+
+function closeAquaCard() {
+    document.getElementById("aquaCard").classList.remove('active');
+}
+
+
+
+function showAuraCard() {
+    document.getElementById("auraCard").classList.add('active');
+
+}
+
+
+function closeAuraCard() {
+    document.getElementById("auraCard").classList.remove('active');
+}
