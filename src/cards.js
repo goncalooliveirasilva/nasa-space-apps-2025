b7543ca107ca8b765b7a34203ad29e9b367ef24d
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-10-05 20:24:19 +0100

    trying fix an error

--- src/cards.js
+++ src/cards.js
@@ -1,23 +1,18 @@
-window.showTerraCard = function () {
+window.showTerraCard = () => {
   document.getElementById('terraCard').classList.add('active')
 }
-
-window.closeTerraCard = function () {
+window.closeTerraCard = () => {
   document.getElementById('terraCard').classList.remove('active')
 }
-
-window.showAquaCard = function () {
+window.showAquaCard = () => {
   document.getElementById('aquaCard').classList.add('active')
 }
-
-window.closeAquaCard = function () {
+window.closeAquaCard = () => {
   document.getElementById('aquaCard').classList.remove('active')
 }
-
-window.showAuraCard = function () {
+window.showAuraCard = () => {
   document.getElementById('auraCard').classList.add('active')
 }
-
-window.closeAuraCard = function () {
+window.closeAuraCard = () => {
   document.getElementById('auraCard').classList.remove('active')
 }
